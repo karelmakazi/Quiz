47be45241e3ea7805a4e6997d282f07ae693b57b
Karel Fourie <karelmakazi@gmail.com>
2020-06-03 15:19:13 +1200

    Deployment

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
